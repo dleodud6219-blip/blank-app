934606e1bed56aaf43136ceaf16ba1f1df5f40cc
dleodud6219-blip <dleodud6219@gmail.com>
2025-08-29 06:07:14 +0000

    타이타닉

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('aa.csv', encoding='cp949')
+
+df.head()
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "한글은 Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
